4d211f64175aa05d22cc3379546ac771c3f93b18
张子航 <1055645963@qq.com>
2020-11-20 21:17:02 +0800

    完善测试

--- src/main/test/zhuang_testTest.java
+++ src/main/test/zhuang_testTest.java
@@ -10,7 +10,8 @@ public class zhuang_testTest {
     public void init() {
         zhuang_test test1 =new zhuang_test();
         int[] arr = test1.init(7);
-        int temp= 1;
+        int temp;
+        temp = 1;
         for(int i=0; i<7; i++)
         {
             if(arr[i]<50&&arr[i]>=0)
